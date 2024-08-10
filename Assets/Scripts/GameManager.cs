8f37ef9bfc7e266d1ec690e1c9f3290a465a1c50
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2024-08-10 10:02:11 -0700

    Fix nullreference for game manager singleton

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,22 +6,26 @@ using UnityEngine.SceneManagement;
 /// Singleton class for global game settings. Persists between scenes and reloads.
 /// </summary>
 public class GameManager : MonoBehaviour {
-    public static GameManager Instance { get; private set; }
+    /// <summary>
+    /// Returns an instance of the Game Manager singleton.
+    /// </summary>
+    public static GameManager Instance
+    {
+        get
+        {
+            if (!_instance)
+                _instance = FindObjectOfType(typeof(GameManager)) as GameManager;
+            DontDestroyOnLoad(_instance?.gameObject);
+            return _instance;
+        }
+    }
+
+    private static GameManager _instance;
 
     /// <summary>
     /// Last checkpoint position. The player should respawn here if they die
     /// </summary>
     [SerializeField] public Vector2 CheckPointPos;
-
-    private void Awake() {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        } else {
-            Destroy(gameObject);
-        }
-    }
 
     /// <summary>
     /// Reloads the current scene
