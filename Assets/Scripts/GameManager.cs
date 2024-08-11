7eb9d4a448ec8af52ff640a6de1e649ffbc82c87
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2024-08-10 19:59:10 -0700

    Set checkpoint pos to player transform by default + remove checkpoint flag

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,9 +12,15 @@ public class GameManager : MonoBehaviour {
     /// </summary>
     public Vector2 CheckPointPos;
 
+    /// <summary>
+    /// Used between scene transitions. Set to false for respawns, true for transition between levels
+    /// </summary>
+    private bool _newLevel;
+
     private void Awake() {
         if (Instance == null)
         {
+            _newLevel = true;
             Instance = this;
             DontDestroyOnLoad(gameObject);
             SceneManager.sceneLoaded += OnSceneLoaded; 
@@ -34,8 +40,15 @@ public class GameManager : MonoBehaviour {
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
         var player = GameObject.FindGameObjectWithTag("Player");
-        Vector3 spawnPos = new Vector3(CheckPointPos.x, CheckPointPos.y, player.transform.position.z);
-        player.transform.position = spawnPos;
+        if (_newLevel == false)
+        {
+            Vector3 spawnPos = new Vector3(CheckPointPos.x, CheckPointPos.y, player.transform.position.z);
+            player.transform.position = spawnPos;
+        }
+        else
+        {
+            CheckPointPos = player.transform.position;
+        }
         
         var cam = GameObject.FindGameObjectWithTag("MainCamera");
         Vector3 camPos = new Vector3(CheckPointPos.x, CheckPointPos.y, cam.transform.position.z);
@@ -44,6 +57,7 @@ public class GameManager : MonoBehaviour {
 
     public void Respawn()
     {
+        _newLevel = false;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
