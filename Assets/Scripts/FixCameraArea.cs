4ffbc4eb01545496708bcff3f8df0dde23c20146
AdenC123 <39308648+AdenC123@users.noreply.github.com>
2024-08-26 18:10:25 -0700

    Use offset for camera position, add to Button prefab

--- Assets/Scripts/FixCameraArea.cs
+++ Assets/Scripts/FixCameraArea.cs
@@ -1,27 +1,29 @@
-using System;
 using UnityEngine;
 
 /// <summary>
 /// Fixes the camera at a set position when the player enters the bounding box.
+/// Camera position is the centre of the box collider, plus an offset.
 /// Uses "MainCamera" and "Player" tags.
 /// </summary>
 public class FixCamera : MonoBehaviour
 {
-    // TODO make this point visible/movable in editor
-    [SerializeField] private Vector2 targetPoint;
+    [SerializeField] private Vector2 targetOffset;
 
     private FollowCamera _cam;
+    private Vector2 _target;
 
     private void Awake()
     {
         _cam = GameObject.FindWithTag("MainCamera").GetComponent<FollowCamera>();
+        Vector2 boxOffset = GetComponent<BoxCollider2D>().offset;
+        _target = (Vector2) transform.position + boxOffset + targetOffset;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            _cam.FixTarget(targetPoint);
+            _cam.FixTarget(_target);
         }
     }
 
