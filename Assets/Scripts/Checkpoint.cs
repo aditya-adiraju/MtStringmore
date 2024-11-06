89f16c0532abca655fa1e485941058a14541a24d
Kai <92591319+kaisoapbox@users.noreply.github.com>
2024-11-06 01:12:59 -0800

    feat: add yarn spinner (#8)
    * feat: add yarn spinner
    
    * feat: dialog trigger on checkpoint flag
    
    * feat: update demo level
    
    * add pangolin font
    
    * Attach new font to prefabs; make the prefabs mutable
    
    * feat: let's mesh that pro text
    
    * feat: add .gitattributes
    
    * feat: use unscaledDeltaTime instead of deltaTime in lineview and dialogue system
    
    * textbox and dialog avatars
    
    * feat: add background sprite to dialog box
    
    * feat: add shadow
    
    * feat: add avatar switching
    
    * feat: tweak padding and continue button position
    
    * feat: checkpoints dont necessarily need yarn nodes
    
    * feat: center the text
    
    * feat: spacebar to skip animation
    
    * fix: style
    
    * fix: redundant code in Checkpoint.cs
    
    ---------
    
    Co-authored-by: jenn1705 <99060936+jentaruno@users.noreply.github.com>

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -1,25 +1,54 @@
-
-using System;
 using UnityEngine;
+using Yarn.Unity;
 
 /// <summary>
-/// Checkpoint flag that sets checkpoint position when player collides with it
+///     Checkpoint flag that sets checkpoint position when player collides with it
 /// </summary>
 public class Checkpoint : MonoBehaviour
 {
-    [Header("References")]
-    [SerializeField] private Animator anim;
+    private static readonly int HoistKey = Animator.StringToHash("Hoisted");
+
+    [Header("References")] [SerializeField]
+    private Animator anim;
+
     [SerializeField] private SpriteRenderer sprite;
     
-    private static readonly int HoistKey = Animator.StringToHash("Hoisted");
- 
+    [Tooltip("Node that starts from this checkpoint. Set to \"\" to not trigger dialog from checkpoint.")]
+    [SerializeField]
+    private string conversationStartNode;
+
+    // internal properties not exposed to editor
+    private DialogueRunner _dialogueRunner;
+    private bool _isCurrentConversation;
+
+    public void Start()
+    {
+        _dialogueRunner = FindObjectOfType<DialogueRunner>();
+        _dialogueRunner.onDialogueComplete.AddListener(EndConversation);
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player") && anim.GetBool(HoistKey) == false)
-        {
-            anim.SetBool(HoistKey, true);
-            GameManager.Instance.CheckPointPos = transform.position;
-        }
+        if (!other.CompareTag("Player") || anim.GetBool(HoistKey)) return;
+        anim.SetBool(HoistKey, true);
+        GameManager.Instance.CheckPointPos = transform.position;
+        StartConversation();
     }
-    
+
+    private void StartConversation()
+    {
+        if (conversationStartNode == "") return;
+        Debug.Log("Started dialogue at checkpoint.");
+        _isCurrentConversation = true;
+        _dialogueRunner.StartDialogue(conversationStartNode);
+        Time.timeScale = 0;
+    }
+
+    private void EndConversation()
+    {
+        if (!_isCurrentConversation) return;
+        _isCurrentConversation = false;
+        Debug.Log("Ended dialogue at checkpoint.");
+        Time.timeScale = 1;
+    }
 }
