7fba07b7448f69e1c4b2d403201da044925a9beb
AdenC123 <39308648+AdenC123@users.noreply.github.com>
2025-05-31 19:45:03 -0700

    Place collectables (#82)
    * simple collectable object
    
    * temp candy graphic
    
    * remove unused function
    
    * collectable sprites
    
    * randomizable collectables
    
    * collectable randomizer menu
    
    * unity moment
    
    * small changes
    
    * WIP collectables in lvl1
    
    * add new collectible sprites and attach yellow ones to collectible prefab
    
    * more collectables
    
    * final level 1 collectables, fix level end screen
    
    * stop complaining about quality settings
    
    * breakable block tuning, red collectable
    
    * place level2 collectables
    
    * remove excess collectables
    
    * randomize level 2 collectables
    
    * collectable sound effect
    
    * disable collider on collect
    
    * new collectable sounds
    
    * use sfx volume mixer
    
    * switch collectable sound
    
    * disable replay monitor
    
    * more tutorial space
    
    * fix early zipper collectables
    
    * update pause and skip button icons
    
    * zipper tutorial space
    
    * version 0.1
    
    ---------
    
    Co-authored-by: Z0rb14n <babybluewoof@gmail.com>
    Co-authored-by: jenn1705 <99060936+jentaruno@users.noreply.github.com>

--- Assets/Scripts/Interactables/Collectable.cs
+++ Assets/Scripts/Interactables/Collectable.cs
@@ -7,16 +7,20 @@ namespace Interactables
     /// <summary>
     /// This object is collected upon collision with the Player, incrementing the total collectable count.
     /// </summary>
-    [RequireComponent(typeof(Collider2D), typeof(SpriteRenderer))]
+    [RequireComponent(typeof(Collider2D), typeof(SpriteRenderer), typeof(AudioSource))]
     public class Collectable : MonoBehaviour
     {
         [SerializeField] private Sprite[] possibleSprites;
         
         private SpriteRenderer _spriteRenderer;
+        private AudioSource _audioSource;
+        private Collider2D _collider;
 
         private void OnValidate()
         {
             _spriteRenderer = GetComponent<SpriteRenderer>();
+            _audioSource = GetComponent<AudioSource>();
+            _collider = GetComponent<Collider2D>();
         }
         
         private void OnTriggerEnter2D(Collider2D other)
@@ -25,7 +29,9 @@ namespace Interactables
             {
                 GameManager.Instance.CollectCollectable(this);
                 // TODO: play a visual/particle effect before destroying
-                Destroy(gameObject);
+                _audioSource.Play();
+                _spriteRenderer.enabled = false;
+                _collider.enabled = false;
             }
         }
 
