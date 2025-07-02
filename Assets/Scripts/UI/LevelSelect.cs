1bda990a7b64efb1d433bdd926564210f8bfa9d1
Jen Taruno <99060936+jentaruno@users.noreply.github.com>
2025-07-02 12:11:57 -0700

    Level select UI tweaks (#95)
    * level select ui tweaks
    
    * fix rider warnings
    
    * del unused level select pngs
    
    * wider right pane
    
    * add "level X" text on right pane
    
    * Set level select menu to inactive on start
    
    level select pop-up was on immediately on start. fixed it
    
    ---------
    
    Co-authored-by: Eddie Yoshie <eddieyoshie@gmail.com>

--- Assets/Scripts/UI/LevelSelect.cs
+++ Assets/Scripts/UI/LevelSelect.cs
@@ -1,118 +1,121 @@
-using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
-using TMPro;
 using System.Collections.Generic;
 using Managers;
 using Save;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class LevelSelectMenu : MonoBehaviour
+namespace UI
 {
-    [SerializeField] GameObject buttonPrefab;
-    [SerializeField] Transform buttonContainer;
-    [SerializeField] Button playButton;
-
-    [SerializeField] Sprite unlockedSprite;
-    [SerializeField] Sprite lockedSprite;
-
-    [SerializeField] List<string> allLevelSceneNames;
-    
-    [SerializeField] private AudioSource canvasAudioSource;
-    [SerializeField] private AudioClip buttonClickSound;
-    [SerializeField] private string level1 = "IntroCutscene";
-    
-    private List<string> unlockedScenes;
-    private string selectedScene;
-    private List<Button> levelButtons = new List<Button>();
-
-    private void Start()
+    public class LevelSelectMenu : MonoBehaviour
     {
-        LazyLoad();
-        unlockedScenes = GameManager.Instance.LevelsAccessed;
-        playButton.interactable = false;
-        unlockedScenes.Add(level1);
+        [SerializeField] GameObject buttonPrefab;
+        [SerializeField] Transform buttonContainer;
+        [SerializeField] Button playButton;
 
-        foreach (string sceneName in allLevelSceneNames)
+        [SerializeField] Sprite unlockedSprite;
+        [SerializeField] Sprite lockedSprite;
+
+        [SerializeField] List<string> allLevelSceneNames;
+    
+        [SerializeField] private AudioSource canvasAudioSource;
+        [SerializeField] private AudioClip buttonClickSound;
+        [SerializeField] private string level1 = "IntroCutscene";
+    
+        private List<string> unlockedScenes;
+        private string selectedScene;
+        private readonly List<Button> levelButtons = new List<Button>();
+
+        private void Start()
         {
-            GameObject btnObj = Instantiate(buttonPrefab, buttonContainer);
-            Button button = btnObj.GetComponent<Button>();
-            TMP_Text label = btnObj.GetComponentInChildren<TMP_Text>();
-            Image background = btnObj.GetComponent<Image>();
+            LazyLoad();
+            unlockedScenes = GameManager.Instance.LevelsAccessed;
+            playButton.interactable = false;
+            unlockedScenes.Add(level1);
 
-            bool isUnlocked = unlockedScenes.Contains(sceneName);
-            int levelNumber = allLevelSceneNames.IndexOf(sceneName) + 1;
+            foreach (string sceneName in allLevelSceneNames)
+            {
+                GameObject btnObj = Instantiate(buttonPrefab, buttonContainer);
+                Button button = btnObj.GetComponent<Button>();
+                TMP_Text label = btnObj.GetComponentInChildren<TMP_Text>();
+                Image background = btnObj.GetComponent<Image>();
+
+                bool isUnlocked = unlockedScenes.Contains(sceneName);
+                int levelNumber = allLevelSceneNames.IndexOf(sceneName) + 1;
             
-            Debug.Log(sceneName + " is " + isUnlocked);
+                Debug.Log(sceneName + " is " + isUnlocked);
 
-            if (isUnlocked)
-            {
-                label.text = levelNumber.ToString();
-                background.sprite = unlockedSprite;
-                button.interactable = true;
-
-                button.onClick.AddListener(() =>
+                if (isUnlocked)
                 {
-                    PlayClickSound();
-                    OnLevelSelected(sceneName, button);
-                });            }
-            else
-            {
-                label.text = "";
-                background.sprite = lockedSprite;
-                button.interactable = false;
+                    label.text = levelNumber.ToString();
+                    background.sprite = unlockedSprite;
+                    button.interactable = true;
+
+                    button.onClick.AddListener(() =>
+                    {
+                        PlayClickSound();
+                        OnLevelSelected(sceneName, button);
+                    });            }
+                else
+                {
+                    label.text = "";
+                    background.sprite = lockedSprite;
+                    button.interactable = false;
+                }
+
+                levelButtons.Add(button);
             }
 
-            levelButtons.Add(button);
+            playButton.onClick.AddListener(OnPlayClicked);
         }
-
-        playButton.onClick.AddListener(OnPlayClicked);
-    }
     
-    private void PlayClickSound()
-    {
-        if (canvasAudioSource != null && buttonClickSound != null)
+        private void PlayClickSound()
         {
-            canvasAudioSource.PlayOneShot(buttonClickSound);
+            if (canvasAudioSource != null && buttonClickSound != null)
+            {
+                canvasAudioSource.PlayOneShot(buttonClickSound);
+            }
         }
-    }
 
-    // <summary>
-    // Ensures GameManager load save data before loading level select menu
-    // </summary>
-    private void LazyLoad()
-    {
-        FindObjectOfType<SaveDataManager>()?.PreloadSaveData();
-    }
-
-    private void OnLevelSelected(string sceneName, Button clickedButton)
-    {
-        selectedScene = sceneName;
-        playButton.interactable = true;
-
-        for (int i = 0; i < levelButtons.Count; i++)
+        // <summary>
+        // Ensures GameManager load save data before loading level select menu
+        // </summary>
+        private void LazyLoad()
         {
-            Image bg = levelButtons[i].GetComponent<Image>();
-            string thisScene = allLevelSceneNames[i];
-            bool isUnlocked = unlockedScenes.Contains(thisScene);
+            FindObjectOfType<SaveDataManager>()?.PreloadSaveData();
+        }
 
-            bg.sprite = isUnlocked ? unlockedSprite : lockedSprite;
+        private void OnLevelSelected(string sceneName, Button clickedButton)
+        {
+            selectedScene = sceneName;
+            playButton.interactable = true;
+
+            for (int i = 0; i < levelButtons.Count; i++)
+            {
+                Image bg = levelButtons[i].GetComponent<Image>();
+                string thisScene = allLevelSceneNames[i];
+                bool isUnlocked = unlockedScenes.Contains(thisScene);
+
+                bg.sprite = isUnlocked ? unlockedSprite : lockedSprite;
             
-            Color color = bg.color;
-            color.a = 1f;
-            bg.color = color;
+                Color color = bg.color;
+                color.a = 1f;
+                bg.color = color;
+            }
+            Image selectedImage = clickedButton.GetComponent<Image>();
+            Color selectedColor = selectedImage.color;
+            selectedColor.a = 0.75f; 
+            selectedImage.color = selectedColor;
         }
-        Image selectedImage = clickedButton.GetComponent<Image>();
-        Color selectedColor = selectedImage.color;
-        selectedColor.a = 0.5f; 
-        selectedImage.color = selectedColor;
-    }
 
-    private void OnPlayClicked()
-    {
-        if (!string.IsNullOrEmpty(selectedScene))
+        private void OnPlayClicked()
         {
-            Debug.Log("Loading selected level: " + selectedScene);
-            SceneManager.LoadScene(selectedScene);
+            if (!string.IsNullOrEmpty(selectedScene))
+            {
+                Debug.Log("Loading selected level: " + selectedScene);
+                SceneManager.LoadScene(selectedScene);
+            }
         }
     }
 }
