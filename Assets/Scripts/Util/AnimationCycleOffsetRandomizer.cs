dbdae1d4b05a094674219c52d0d9ab4d953b44aa
Jen Taruno <99060936+jentaruno@users.noreply.github.com>
2025-08-03 19:39:08 -0700

    Hot chocolate updates (#118)
    * steeeem
    
    * tweak params
    
    * remove old unused steam png
    
    * death area bloop bloop
    
    * move em
    
    * crop steam

---
+++ Assets/Scripts/Util/AnimationCycleOffsetRandomizer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Util
+{
+    [RequireComponent(typeof(Animator))]
+    public class AnimationCycleOffsetRandomizer : MonoBehaviour
+    {
+        private Animator anim;
+        private static readonly int CycleOffsetKey = Animator.StringToHash("CycleOffset");
+        
+        private void Awake()
+        {
+            anim = GetComponent<Animator>();
+            anim.SetFloat(CycleOffsetKey, Random.Range(0f, 1f));
+        }
+    }
+}
